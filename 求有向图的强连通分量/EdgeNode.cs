ae26eddc3f84e96709d3f1462e628d783fe02675
wangqing <wangqing@corp-ci.com>
2016-03-25 16:34:31 +0800

    设置顶点个数窗体的设计，节点类的添加

--- 求有向图的强连通分量/EdgeNode.cs
+++ 求有向图的强连通分量/EdgeNode.cs
@@ -8,6 +8,9 @@ namespace 求有向图的强连通分量
 {
     class EdgeNode
     {
-
+        private int tailvex;
+        private int headvex;
+        private EdgeNode headlink;
+        private EdgeNode taillink;
     }
 }
--- 求有向图的强连通分量/Form1.Designer.cs
+++ 求有向图的强连通分量/Form1.Designer.cs
@@ -28,20 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.vertexnum = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.vertexnum)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "顶点个数:";
+            // 
+            // vertexnum
+            // 
+            this.vertexnum.Font = new System.Drawing.Font("宋体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.vertexnum.Location = new System.Drawing.Point(112, 7);
+            this.vertexnum.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.vertexnum.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.vertexnum.Name = "vertexnum";
+            this.vertexnum.ReadOnly = true;
+            this.vertexnum.Size = new System.Drawing.Size(46, 29);
+            this.vertexnum.TabIndex = 3;
+            this.vertexnum.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "确定";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(504, 362);
+            this.ClientSize = new System.Drawing.Size(172, 83);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.vertexnum);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.vertexnum)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown vertexnum;
+        private System.Windows.Forms.Button button1;
     }
 }
 
