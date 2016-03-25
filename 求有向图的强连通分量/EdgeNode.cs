0f8c18a2a42b9f4f7aefbf4e434c7b7d53900982
wangqing <wangqing@corp-ci.com>
2016-03-25 14:41:28 +0800

    初始版本

---
+++ 求有向图的强连通分量/EdgeNode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 求有向图的强连通分量
+{
+    class EdgeNode
+    {
+
+    }
+}
---
+++ 求有向图的强连通分量/VertexNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 求有向图的强连通分量
+{
+    class VertexNode
+    {
+        private EdgeNode firstin;
+        private EdgeNode firstout;
+    }
+}
